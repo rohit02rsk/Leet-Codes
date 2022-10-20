65239265792aee038f2a8bdeac0410ab61b16bf9
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2022-10-20 10:18:38 +0530

    Time: 18 ms (9.65%), Space: 9.1 MB (82.34%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -9,7 +9,7 @@ public:
         string first = str[0];
         string last = str[n - 1];
         
-        for (int i=0; i < first.size(); i++) {
+        for (int i=0 ; i < first.size() ; i++) {
             if (first[i] == last[i]) res += first[i];
             else break;
         }
