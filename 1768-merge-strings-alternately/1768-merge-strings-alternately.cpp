76039ec0305896574499175ebccd4c32157ccb63
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-04-18 18:21:14 +0530

    Time: 4 ms (38.78%), Space: 6.4 MB (19.57%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int len = min(word1.length(), word2.length());
+        int i=0;
+        for(; i<len; i++) {
+            res += word1[i];
+            res += word2[i];
+        }
+        if(word1.length() < word2.length()) {
+            while(i<word2.length()) {
+                res += word2[i];
+                i++;
+            }
+        } else if (word2.length() < word1.length()) {
+            while(i<word1.length()) {
+                res += word1[i];
+                i++;
+            }
+        }
+        return res;
+    }
+};
