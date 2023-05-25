09b9a5005c5354022c270fd6f5dc4264847e2c9c
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-05-25 16:17:58 +0530

    Time: 194 ms (25.68%), Space: 62.8 MB (92.81%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        long long top_sum = grid[0][0], b_sum = 0, res = LONG_MAX;
+        long long top_sum = grid[0][0], bot_sum = 0, res = LONG_MAX;
         for(int i = 1; i<grid[0].size(); i++)
             top_sum += grid[0][i];
         
         for(int i =0; i<grid[0].size(); i++) {
             top_sum -= grid[0][i];
-            res = min(res, max(top_sum, b_sum));
-            b_sum += grid[1][i];
+            res = min(res, max(top_sum, bot_sum));
+            bot_sum += grid[1][i];
         }
 
         return res;
