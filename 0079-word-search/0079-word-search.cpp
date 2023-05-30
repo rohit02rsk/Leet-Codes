4e241b6dc69cfd18b442101ea31e294e2507cd7e
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-05-30 17:36:11 +0530

    Time: 1150 ms (17.42%), Space: 7.9 MB (96.78%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int ind) {
+        if(ind == word.size())
+            return true;
+        
+        if(i < 0 or j < 0
+        or i >= board.size()
+        or j >= board[0].size()
+        or word[ind] != board[i][j])
+            return false;
+
+        char c = board[i][j];
+        board[i][j] = '*';
+        bool res = dfs(board, word, i+1, j, ind+1)
+                or dfs(board, word, i-1, j, ind+1)
+                or dfs(board, word, i, j+1, ind+1)
+                or dfs(board, word, i, j-1, ind+1);
+        board[i][j] = c;
+        
+        return res;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(dfs(board, i, j, word))
+        for(int i=0; i<board.size(); i++) {
+            for(int j=0; j<board[0].size(); j++) {
+                if(dfs(board, word, i, j, 0))
                     return true;
             }
         }
         return false;
-    }
-    bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-        if (!word.size())
-            return true;
-        if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
-            return false;
-        char c = board[i][j];
-        board[i][j] = '*';
-        string s = word.substr(1);
-        bool ret = dfs(board, i-1, j, s) || 
-                   dfs(board, i+1, j, s) || 
-                   dfs(board, i, j-1, s) || 
-                   dfs(board, i, j+1, s);
-        board[i][j] = c;
-        return ret;
     }
 };
