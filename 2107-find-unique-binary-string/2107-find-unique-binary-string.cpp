81fbfa7534e761bd9481081696715d3367f177d1
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-06-01 15:44:43 +0530

    Time: 1243 ms (10.53%), Space: 160 MB (9.01%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    string help(unordered_set<string>& s, int n, string curr) {
+    string res = "";
+    void help(unordered_set<string>& s, int n, string curr) {
         if(curr.size() == n) {
-            if(!s.count(curr))
-                return curr;
-            else
-                return "";
+            if(!s.count(curr)) {
+                res = curr;
+                return;
+            }
+            else return;
         }
-        string temp = help(s, n, curr+"0");
-        string temp1 = help(s, n, curr+"1");
-        return temp == "" ? temp1 : temp;
+        help(s, n, curr+"0");
+        help(s, n, curr+"1");
     }
     string findDifferentBinaryString(vector<string>& nums) {
         unordered_set<string> s(nums.begin(), nums.end());
-        return help(s, nums.size(), "");
+        help(s, nums.size(), "");
+        return res;
     }
 };
