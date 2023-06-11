f8ce090d026d4cd3c5975238401aaf1dc6d50d13
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-06-11 13:46:33 +0530

    Time: 103 ms (50%), Space: 40 MB (16.67%) - LeetHub

---
+++ 2828-lexicographically-smallest-string-after-substring-operation/2828-lexicographically-smallest-string-after-substring-operation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string smallestString(string s) {
+        int n = s.size();
+        int i = 0;
+        while(i < n && s[i] == 'a') 
+            i++;
+        if(i >= n) { 
+            s[n-1] = 'z'; 
+            return s; 
+        }
+        while(i < n && s[i] != 'a') { 
+            s[i]--; 
+            i++; 
+        }
+        return s;
+    }
+};
