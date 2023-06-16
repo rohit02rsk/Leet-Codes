90f6ae834332cc907ef025d37c46a34231bb2518
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-06-16 15:23:28 +0530

    Time: 46 ms (84.43%), Space: 16.1 MB (41.51%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    map<pair<int, int>, int> dp; // {i, j} -> max len
-    
-    int dfs(vector<vector<int>>& matrix, int i, int j, int m, int n) {
-        if(i < 0 or j < 0 or i >= m or j >= n)
-            return 1;
+    int dfs(int i, int j, int pre, int m, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        if(i<0 or j<0 or i>=m or j>=n or pre >= matrix[i][j])
+            return 0;
         
-        if(dp.find({i, j}) != dp.end()) 
-            return dp[{i, j}];
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        int LIP = 0;
-        
-        vector<vector<int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        for(int it=0; it<4; it++) {
-            int x = i + dir[it][0];
-            int y = j + dir[it][1];
-            if(x>=0 and y>=0 and x<m and y<n and matrix[x][y] > matrix[i][j]) {
-                LIP = max(LIP, 1 + dfs(matrix, x, y, m, n));
-            }
-        }
+        int l = dfs(i, j-1, matrix[i][j], m, n, matrix, dp);
+        int r = dfs(i, j+1, matrix[i][j], m, n, matrix, dp);
+        int u = dfs(i-1, j, matrix[i][j], m, n, matrix, dp);
+        int d = dfs(i+1, j, matrix[i][j], m, n, matrix, dp);
 
-        dp[{i, j}] = LIP;
-        return LIP;
+        return dp[i][j] = max(max(l, r), max(u, d)) + 1;
     }
-
+    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
         int res = 0;
-        
-        for(int i=0; i<m; i++) 
-            for(int j=0; j<n; j++) 
-                res = max(res, 1 + dfs(matrix, i, j, m, n));
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                res = max(res, dfs(i, j, -1, m, n, matrix, dp));
+            }
+        }
 
         return res;
     }
