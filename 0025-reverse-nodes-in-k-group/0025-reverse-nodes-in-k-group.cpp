3ce635826a97daafe1df71cf80cb0d09fe3c975d
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-06-26 12:31:10 +0530

    Time: 10 ms (97.15%), Space: 11.3 MB (91.08%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,37 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* getKthNode(ListNode* curr, int k) {
+        while(curr and k--) curr = curr->next;
+        return curr;
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* dummy = new ListNode(-1, head);
-        
-        ListNode* prev = dummy;
-        ListNode* curr = dummy->next;
-        ListNode* temp = NULL;
-        
-        int count = k;
-        
-        while (curr != NULL) {
-            if (count > 1) {
-                temp = prev->next;
-                prev->next = curr->next;
-                curr->next = curr->next->next;
-                prev->next->next = temp;
-
-                count--;
-            } else {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* grpPrev = dummy;
+        while (1) {
+            ListNode* kth = getKthNode(grpPrev, k);
+            if(!kth) return dummy->next;
+            ListNode* grpNext = kth->next;
+            
+            // reverse the group
+            ListNode* prev = kth->next;
+            ListNode* curr = grpPrev->next;
+            while(curr != grpNext) {
+                ListNode* temp = curr->next;
+                curr->next = prev;
                 prev = curr;
-                curr = curr->next;
-                count = k;
-                
-                ListNode* end = curr;
-                for (int i = 0; i < k; i++) {
-                    if (end == NULL) {
-                        return dummy->next;
-                    }
-                    end = end->next;
-                }
+                curr = temp;
             }
+            ListNode* temp = grpPrev->next;
+            grpPrev->next = kth;
+            grpPrev = temp;
         }
-        
-        return dummy->next;
+        return NULL;
     }
 };
