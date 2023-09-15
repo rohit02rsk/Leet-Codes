479aeacbdb6ee23e48b75fc2e6ccfd2cc195ed6a
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-09-15 13:12:45 +0530

    Time: 177 ms (79.11%), Space: 47.2 MB (75.96%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -3,32 +3,34 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
         unordered_set<int> vis;
-        set<pair<int, int>> minHeap;
-        vector<vector<int>> adj(n, vector<int>(n));
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
+        int adj[n][n];
         int res = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                adj[i][j] = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[j][i] = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-            }
-        }
-        minHeap.insert({0, 0});
-        while(vis.size() < n) {
-            auto [cost, curr] = *minHeap.begin();
-            minHeap.erase(minHeap.begin());
-            
-            if(!vis.count(curr)) {
-                res += cost;
-                vis.insert(curr);
-            }
-            else continue;
 
-            for(int i=0; i<n; i++) {
-                if(!vis.count(i)) {
-                    minHeap.insert({adj[i][curr], i});
-                }
+        for(int i=0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+                int tmp = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                adj[i][j] = tmp;
+                adj[j][i] = tmp;
             }
         }
+
+        pq.push({0, 0});
+        while(vis.size() != n) {
+            auto [cost, curr] = pq.top();
+            pq.pop();
+            
+            if(vis.count(curr))
+                continue;
+            
+            res += cost;
+            vis.insert(curr);
+
+            for(int i=0; i<n; i++)
+                if(!vis.count(i))
+                    pq.push({adj[curr][i], i}); 
+        }
+
         return res;
     }
 };
