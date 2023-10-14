3acdbdadf9fde4e0470c2b55ea02c53ebed80a2a
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2023-10-14 09:32:16 +0530

    Time: 740 ms (6.09%), Space: 350.9 MB (6.4%) - LeetHub

---
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int f(vector<int>& cost, vector<int>& time, int ind, int rem) {
+        if (rem <= 0) return 0;
+        if (ind == time.size()) return 1e9;
+        if (dp[ind][rem] != -1) return dp[ind][rem];
+        return dp[ind][rem] = min(
+            f(cost, time, ind+1, rem),
+            cost[ind] + f(cost, time, ind+1, rem - time[ind] - 1)
+        );
+    }
+    int paintWalls(vector<int>& cost, vector<int>& time) {
+        dp.resize(501, vector<int>(501,-1));
+        return f(cost, time, 0, time.size());
+    }
+};
